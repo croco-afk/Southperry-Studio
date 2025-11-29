d41b661d8d0e459c129070758cb74cecba62c4fe
X.J.W. <amyfor7@gmail.com>
2025-11-29 20:37:41 +0800

    chore: setup dev environment for using typescript

---
+++ src/env.d.ts
@@ -0,0 +1,11 @@
+/// <reference types="vite/client" />
+
+declare module '*.vue' {
+  import type { DefineComponent } from 'vue'
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/ban-types
+  const component: DefineComponent<{}, {}, any>
+  export default component
+}
+
+// 如果你的 JS 项目中有全局变量，也可以在这里声明
+// declare const __LANG__: string;
